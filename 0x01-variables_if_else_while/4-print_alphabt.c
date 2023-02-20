ff7fca39f482744b9e4b1388193351c2c0fe29a2
Mesomamadu <mesomamadu2004@gmail.com>
2023-02-20 06:04:22 -0800

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - Entruy point
+  *
+  * Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	char C;
+
+	c = 'a';
+	while
+		(c <= 'z')
+	{
+		if ((c != 'q' && c != 'e') && c <= 'z'
+			putchar(c);
+			c++;
+	}
+	putchar('\n');
+	return (0);
+}
