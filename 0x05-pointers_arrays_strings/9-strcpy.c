1c02a943c699eaf5524a997f6d8f89e828b8b6ca
Mesomamadu <mesomamadu2004@gmail.com>
2023-03-02 09:01:06 -0800

    update

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,8 +13,8 @@ char *_strcpy(char *dest, char *src)
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		*(dest + 1) = *(src + 1);
+		*(dest + i) = *(src + i);
 	}
-	*(dest + 1) = '\0';
+	*(dest + i) = '\0';
 	return (dest);
 }
