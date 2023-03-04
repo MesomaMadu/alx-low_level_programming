3f560f6be5b0b75743c4993819d09afc9664fd9f
Mesomamadu <mesomamadu2004@gmail.com>
2023-03-04 11:04:51 -0800

    update

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+
+/**
+ * main - Entry point
+ * write your line of code here...
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - Only one statement
+ * - You are not allowed to code anything else than this line of code
+ */
+	*(p + 5) = 98;
+/* ...so thar this prints 98\n */
+	printf(a"[2] = %d\n", a[2]);
+	return (0);
+}
