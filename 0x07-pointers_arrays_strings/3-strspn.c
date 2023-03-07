4085969bfb99006eb758a972b16510e78feb2631
Mesomamadu <mesomamadu2004@gmail.com>
2023-03-06 17:46:17 -0800

    pointers and arrays

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - function that searches a string
+ * @s: input
+ * @accept: input
+ *
+ * Return: Always 0 (success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int r;
+
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
