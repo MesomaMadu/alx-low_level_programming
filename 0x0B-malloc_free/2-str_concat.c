fcc44ffce190afd762f6c7637e399d09f5f27223
Mesomamadu <mesomamadu2004@gmail.com>
2023-03-16 10:39:31 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -40,8 +40,8 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
-	for (j = 0; s2[j] != '\0'; i++, j++)
-		ptr[j] = s2[j];
+	for (j = 0; s2[j] != '\0'; j++, i++)
+		ptr[i] = s2[j];
 
 	ptr[i] = ('\0');
 	return (ptr);
