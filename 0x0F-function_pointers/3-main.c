e90884790bf376702135bc3469af8e0f47a336d4
Mesomamadu <mesomamadu2004@gmail.com>
2023-03-23 15:17:39 -0700

    function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,7 +8,7 @@
  * @argv: An array of pointers to the arguements
  * Return: Always 0.
  */
-int main(int __attribute__(__unused__)argc, char *argv[])
+int main(int __attribute__((__unused__))argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
