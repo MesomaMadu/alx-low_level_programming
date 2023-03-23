809fe6969fd287fc4987d747a076bae484c1f931
Mesomamadu <mesomamadu2004@gmail.com>
2023-03-23 14:30:14 -0700

    functton_pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,9 +4,9 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-void print_name(char *name, void(*f)(char*);
+void print_name(char *name, void(*f)(char*));
 int _putchar(char c);
-void array_iterator(int *array, size t_size, void(*action)(int));
+void array_iterator(int *array, size_t size, void(*action)(int));
 int int_index(int *array, int size, int(*cmp)(int));
 
 #endif
