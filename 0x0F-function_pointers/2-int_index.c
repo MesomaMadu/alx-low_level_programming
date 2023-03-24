2b6f840afcb07b2e94b81577c31f2388d5720e0d
Mesomamadu <mesomamadu2004@gmail.com>
2023-03-23 18:04:29 -0700

    function_pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,9 +1,10 @@
 #include "function_pointers.h"
 /**
  * int_index - return index place if comparison = true, else -1
- * @a: array
+ * @array: array
  * @size: size of elements in array
- * @cmp: pointer to func of one of the 3 in main
+ * @cmp: pointer to func
+ * of one of the 3 in main
  * Return: 0
  */
 int int_index(int *array, int size, int(*cmp)(int))
--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,16 +1,16 @@
 #ifndef CALC_H
 #define CALC_H
 /**
- * struct op - A struct op
+ * struct op - Struct op
+ *
  * @op: The operator
- * @f: the associated function
+ * @f: The function associated
  */
 typedef struct op
 {
-	char *op;
-	int (*f)(int a, int b);
-}
-op_t;
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
