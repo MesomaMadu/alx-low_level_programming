5a9108e9637befb382e8d6976d647e1fffa7bf3e
Mesomamadu <mesomamadu2004@gmail.com>
2023-03-23 18:07:58 -0700

    function_pointers

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -10,7 +10,8 @@ typedef struct op
 {
     char *op;
     int (*f)(int a, int b);
-} op_t;
+}
+op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,7 +18,7 @@ int (*get_op_func(char *s))(int, int)
 	{NULL, NULL}
 	};
 
-	int i = 0;
+	int i;
 
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{
