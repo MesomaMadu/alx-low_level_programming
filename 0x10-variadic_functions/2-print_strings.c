8aac00de039ca9b5a2f3c87177ffd1d08b7a773d
Mesomamadu <mesomamadu2004@gmail.com>
2023-03-25 03:37:49 -0700

    variadic_functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		str = va_arg(strings, char *);
 
 		if (str == NULL)
-			printf( "(nil)");
+			printf("(nil)");
 
 		else
 			printf("%s", str);
