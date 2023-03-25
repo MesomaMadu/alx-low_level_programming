ab9770d756a89cb7aaf12037022b3340e4141e52
Mesomamadu <mesomamadu2004@gmail.com>
2023-03-25 03:50:38 -0700

    variadic_functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", str);
 
-		if (index != (n - 1) && separator == NULL)
+		if (index != (n - 1) && separator != NULL)
 		printf("%s", separator);
 	}
 	printf("\n");
